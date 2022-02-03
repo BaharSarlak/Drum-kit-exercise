3845d715adaa1e940a5b29220e9e2e1c8f76e498
BaharSarlak <bsarlak98@gmail.com>
2022-02-02 19:52:34 -0500

    animations on buttons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ for(let i=0; i<document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         const target=this.innerHTML
         playAudio(target)
+        buttonAnimaion(target)
     });
 
 }
@@ -10,44 +11,45 @@ for(let i=0; i<document.querySelectorAll(".drum").length; i++) {
 document.addEventListener("keydown", event=>{
     let target=event.key;
     playAudio(target);
+    buttonAnimaion(target)
 });
+
+function buttonAnimaion(key) {
+    document.querySelector('.'+key).classList.add('pressed')
+    setTimeout(()=>{
+        document.querySelector('.'+key).classList.remove('pressed')
+    }, 200)
+}
 
 function playAudio(key) {
     switch (key) {
         case "w":
             const ton1= new Audio('./sounds/tom-1.mp3');
             ton1.play();
-            this.style.color='white';        
             break;
         case "a":
             const ton2= new Audio('./sounds/tom-2.mp3');
             ton2.play();
-            this.style.color='white';        
             break;
         case "s":
             const ton3= new Audio('./sounds/tom-3.mp3');
             ton3.play();
-            this.style.color='white';        
             break;
         case "d":
             const ton4= new Audio('./sounds/tom-4.mp3');
             ton4.play();
-            this.style.color='white';        
             break;
         case "j":
             const ton5= new Audio('./sounds/crash.mp3');
             ton5.play();
-            this.style.color='white';        
             break;
         case "k":
             const ton6= new Audio('./sounds/kick-bass.mp3');
             ton6.play();
-            this.style.color='white';        
             break;
         case "l":
             const ton7= new Audio('./sounds/snare.mp3');
             ton7.play();
-            this.style.color='white';        
             break;       
         default: break;
     }
